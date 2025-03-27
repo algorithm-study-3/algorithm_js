db68640634a536ea22e20c3b0a609e8d51440a84
deokbin <deokbin@naver.com>
2025-03-27 10:48:45 +0900

    03.24~03.27 알고리즘 풀이

---
+++ 03.27/03.27(1).js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+  const set1 = new Set(nums1);
+
+  for (const num of nums2) {
+    if (set1.has(num)) return num;
+  }
+  return -1;
+};
